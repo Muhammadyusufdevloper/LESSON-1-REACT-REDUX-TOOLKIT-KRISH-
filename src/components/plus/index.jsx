f5d2b083f6f28cb9aa63864930c07b4d0f91c336
Muhammadyusufdeveloper <155054579+Muhammadyusufdevloper@users.noreply.github.com>
2024-05-31 00:00:04 +0500

    home

--- src/components/plus/index.jsx
+++ src/components/plus/index.jsx
@@ -1,24 +1,25 @@
 import { CiCirclePlus } from "react-icons/ci";
 import "./Plus.scss";
 import { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { increment } from "../../context/slice/countSlice";
 
 const Plus = () => {
-    // const count = useSelector((state) => state.count.value);
-    const [inc, setInc] = useState("");
+    const count = useSelector((state) => state.count.value);
+    const [inc, setInc] = useState(localStorage.getItem("count") || "");
     const dispatch = useDispatch();
 
 
     return (
         <div className="plus">
             <input
+                maxLength={10}
                 value={inc}
                 onChange={(e) => setInc(e.target.value)}
                 type="number"
                 placeholder="Enter number"
             />
-            <button onClick={() => dispatch(increment(+inc || 1))} className="plus__btn">
+            <button disabled={count >= 100000000} onClick={() => dispatch(increment(+inc || 1))} className="plus__btn">
                 <CiCirclePlus />
             </button>
         </div>
